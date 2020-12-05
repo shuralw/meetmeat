8a473578f1e14260772e949f740b53bac26c7bd0
Janna <janna.tholen@ewetel.net>
2020-12-05 14:58:08 +0100

    added card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { StartComponent } from './start/start.component';
 import { UeberunsComponent } from './ueberuns/ueberuns.component';
 import { BratwurstproComponent } from './bratwurstpro/bratwurstpro.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { BratwurstproComponent } from './bratwurstpro/bratwurstpro.component';
     StartComponent,
     UeberunsComponent,
     BratwurstproComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -12,5 +12,4 @@ export class StartComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  
 }

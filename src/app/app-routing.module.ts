25fc92a98dc44dda812a00dec45ce293ca380eb3
Janna <janna.tholen@ewetel.net>
2020-12-08 21:17:03 +0100

    Einfügen Impressum

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
 import { BratwurstproComponent } from './bratwurstpro/bratwurstpro.component';
 import { StartComponent } from './start/start.component';
 import { UeberunsComponent } from './ueberuns/ueberuns.component';
